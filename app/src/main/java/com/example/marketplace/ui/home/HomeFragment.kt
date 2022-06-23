e8d6a5c5d223333b26826f74781ad76a51257174
humaidi <humaidizakaria1998@gmail.com>
2022-06-24 00:43:19 +0700

    Implementasi MVVM arsitektur

--- app/src/main/java/com/example/marketplace/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/marketplace/ui/home/HomeFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.marketplace.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +19,28 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setData()
+        getData()
         return root
+    }
+
+    private fun setData() {
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            binding.tvSelamat.text = it
+        }
+    }
+
+
+    private fun getData(){
+        binding.btnKlik.setOnClickListener {
+           homeViewModel.ubahData()
+        }
     }
 
     override fun onDestroyView() {
